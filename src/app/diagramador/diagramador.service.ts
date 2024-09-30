a66ed2c94546ae53bb3db5771c1d142b09b5ab5d
crayolito <josalejandroapp2017@gmail.com>
2024-09-30 17:29:44 -0400

    vfrontend software1v

---
+++ src/app/diagramador/diagramador.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
+import { AuthService } from '../auth/auth.service';
+import { WebsocketService } from '../common/services/websocket.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DiagramadorService {
+  private apiUrl = environment.apiUrl;
+  public http = inject(HttpClient);
+  public wsService = inject(WebsocketService);
+  public userAuth = inject(AuthService);
+
+  onListenChangedDiagrama() {
+    return this.wsService.listen('changed-diagrama');
+  }
+
+  emitChangedDiagrama(diagrama: string) {
+    this.wsService.emit('changed-diagrama', {
+      id: this.userAuth.getSalaDiagrama()!.id,
+      diagrama,
+    });
+  }
+
+  contenidoVerifDiagramaBD(nombreSala: string): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}/salas/` + nombreSala);
+  }
+}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://backend-sw1-19e3145c5188.herokuapp.com',
+  wsUrl: 'https://backend-sw1-19e3145c5188.herokuapp.com',
+};
