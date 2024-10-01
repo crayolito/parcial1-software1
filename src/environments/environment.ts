71fe54a901632e9d8b8d5241ac50c7106ac2ab3b
crayolito <josalejandroapp2017@gmail.com>
2024-10-01 16:21:40 -0400

    ultima actualizacion

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  wsUrl: 'http://localhost:3000',
+  apiUrl: 'https://examen-parcial1-909d2c6f6a8d.herokuapp.com',
+  wsUrl: 'https://examen-parcial1-909d2c6f6a8d.herokuapp.com',
 };
