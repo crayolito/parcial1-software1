4cba835c0091c247ea06c764b7594bbd9a69b9ac
crayolito <josalejandroapp2017@gmail.com>
2024-10-04 14:23:42 -0400

    examen hoy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000',
-  wsUrl: 'http://localhost:3000',
+  apiUrl: 'https://examen-parcial1-909d2c6f6a8d.herokuapp.com',
+  wsUrl: 'https://examen-parcial1-909d2c6f6a8d.herokuapp.com',
 };
 
 // https://examen-parcial1-909d2c6f6a8d.herokuapp.com
